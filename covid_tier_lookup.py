1b2beeec6bd1c70c353b21bbfacf88e43ae83aad
Adam Granger <adam@adamish.com>
2020-11-06 21:06:22 +0000

    fix for national lockdown

--- covid_tier_lookup.py
+++ covid_tier_lookup.py
@@ -1,5 +1,6 @@
 import urllib.request
 import re
+from urllib.error import HTTPError
 
 class CovidTierLookup:
 	# lookup tier by postcode and return zero indexed tier via callback, -1 if unknown
@@ -8,20 +9,22 @@ class CovidTierLookup:
 		values['postcode-lookup'] = postcode
 		data = urllib.parse.urlencode(values)
 		data = data.encode('ascii')
-		req = urllib.request.Request('https://www.gov.uk/find-coronavirus-local-restrictions', data)
+		try:
+			req = urllib.request.Request('https://www.gov.uk/find-coronavirus-local-restrictions', data)
 
-		with urllib.request.urlopen(req) as response:
-			html = response.read().decode('utf-8').replace('\n', ' ').replace('\r', '')
-			m = re.search('COVID alert level: (.*?)<', html, re.MULTILINE | re.IGNORECASE)
-			result = -1;
-			if m:
-				level = m.group(1).strip().lower();
-				if level.startswith('medium'):
-					result = 0
-				elif level.startswith('high'):
-					result = 1
-				elif level.startswith('very high'):
-					result = 2
+			with urllib.request.urlopen(req) as response:
+				html = response.read().decode('utf-8').replace('\n', ' ').replace('\r', '')
+				m = re.search('COVID alert level: (.*?)<', html, re.MULTILINE | re.IGNORECASE)
+				result = -1;
+				if m:
+					level = m.group(1).strip().lower();
+					if level.startswith('medium'):
+						result = 0
+					elif level.startswith('high'):
+						result = 1
+					elif level.startswith('very high'):
+						result = 2
 
-				resultCallback(result)
-
+					resultCallback(result)
+		except HTTPError as err:
+			resultCallback(-1)
